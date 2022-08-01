8b9078f2702f3ef1884b1e3f4a57d76e6c68eca7
Ayush Choubey <ayush.choubey@gmail.com>
2022-08-01 13:32:57 +0800

    add BPA to s3 bucket (#46)

--- main.tf
+++ main.tf
@@ -656,6 +656,15 @@ resource "aws_s3_bucket" "flowlogs_to_s3" {
   )
 }
 
+resource "aws_s3_bucket_public_access_block" "flowlogs_to_s3" {
+  bucket = aws_s3_bucket.flowlogs_to_s3.id
+
+  block_public_acls        = true
+  block_public_policy      = true
+  ignore_public_acls       = true
+  restrict_public_buckets  = true
+}
+
 resource "aws_s3_bucket_policy" "flowlogs_to_s3" {
   bucket = aws_s3_bucket.flowlogs_to_s3.id
   policy = data.aws_iam_policy_document.flowlogs_to_s3.json
